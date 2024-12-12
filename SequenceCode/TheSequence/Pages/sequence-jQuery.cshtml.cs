0014679e9411f7418e4d92c17374699d15d4091a
chavywal <wchavyw@gmail.com>
2024-12-12 14:06:02 -0500

    #11

---
+++ SequenceCode/TheSequence/Pages/sequence-jQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheSequence.Pages
+{
+    public class sequence_jQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
